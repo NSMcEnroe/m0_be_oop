ffbadfc2543075c22ec05459fac614c81e4f7829
Nicholas McEnroe <nsmcenro@gmail.com>
2023-06-01 22:45:15 -0400

    Add Third challenge

--- oop.rb
+++ oop.rb
@@ -23,6 +23,25 @@ p unicorn1.say
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
 
+class Vampire
+    attr_reader :name, :pet, :thirsty
+    def initialize(name, pet = "bat")
+        @name = name
+        @pet = pet
+        @thirsty = true
+    end
+
+    def drink
+        @thirsty = false
+        print "#{name} is no longer thirsty."
+    end
+
+end
+
+vampire1 = Vampire.new("Vlad")
+puts vampire1.drink
+
+p vampire1
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -31,6 +50,25 @@ p unicorn1.say
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
 
+class Dragon
+    attr_reader :name, :rider, :color
+    def initialize(name, rider, color)
+        @name = name
+        @rider = rider
+        @color = color
+        @is_hungry = false
+    end
+
+    def eat 
+        @is_hungry = false
+    end
+end
+
+dragon1 = Dragon.new("Henry", "Corey", "Red")
+p dragon1.eat
+p dragon1
+
+#Couldn't figure out how to execute the method "eat" multiple times while getting different inputs.  I think I could probably do this in an array, but there must be an easier way to do this.
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -40,3 +78,4 @@ p unicorn1.say
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
